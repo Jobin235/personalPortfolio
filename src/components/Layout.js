3c5712bfe36c7d16dd49d80cd95310c0c0107af6
Jobin Joy <jobinjoy235@gmail.com>
2023-06-27 18:15:51 +0530

    back to menu button added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {VerticleButton as ScrollUpButton} from "react-scroll-up-button";
 import SideMenu from "../components/SideMenu";
+import MenuButton from "../utils/MenuButton";
 import styles from "../styles/components/Layout.module.css";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
+      
       <SideMenu className={styles.sidemenu}/>
+      <MenuButton/>
       <main>{children}</main>
       <ScrollUpButton />
     </div>
