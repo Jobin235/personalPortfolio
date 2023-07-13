2de084e9dbf97826e346d38a16dce2fc9d132324
Jobin Joy <jobinjoy235@gmail.com>
2023-07-13 12:42:19 +0530

    portfolio page made responsive

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,10 +3,24 @@ import React from "react";
 import HeaderText from "../components/HeaderText";
 import styles from "../styles/pages/Contact.module.css";
 
+import PageTransition from "../components/PageTransition";
+import Reveal from "../components/Reveal";
+
 export default function ContactPage() {
   return (
     <div className={styles.container}>
-      <HeaderText text="Contact"/>
+      <HeaderText text="Contact" />
+      <PageTransition>
+        <div className={styles.content}>
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <div className={styles.details}>
+                hi
+              </div>
+            </div>
+          </div>
+        </div>
+      </PageTransition>
     </div>
   );
 }
