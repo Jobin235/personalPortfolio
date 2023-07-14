eb0a6437f7e9916f162e620b4bd7bd1c4dd46a7f
Jobin Joy <jobinjoy235@gmail.com>
2023-07-14 05:39:41 +0530

    Contacts Page added

---
+++ src/data/contact.js
@@ -0,0 +1,83 @@
+export const contactData = [
+  {
+    logo: require("../assets/images/phone.png"),
+    type: "Phone",
+    detail: "+91 9495322806",
+  },
+  {
+    logo: require("../assets/images/email.png"),
+    type: "Gmail",
+    detail: "jobinjoy235@gmail.com",
+  },
+  {
+    logo: require("../assets/images/linkedin.png"),
+    type: "Linkedin",
+    detail: "jobin0602",
+    link: "https://in.linkedin.com/in/jobin0602",
+  },
+  {
+    logo: require("../assets/images/github.png"),
+    type: "Github",
+    detail: "Jobin235",
+    link: "https://github.com/Jobin235",
+  },
+];
+
+export const infoData = [
+  {
+    placeholder: "First Name",
+    name: "first_name",
+  },
+  {
+    placeholder: "Last Name",
+    name: "last_name",
+  },
+  {
+    placeholder: "Email address",
+    name: "email",
+  },
+  {
+    placeholder: "Company Name",
+    name: "comapny",
+  },
+  {
+    placeholder: "Phone Number",
+    name: "phone",
+  },
+  {
+    placeholder: "Country",
+    name: "country",
+  },
+];
+export const serviceData = [
+  {
+    value: "development",
+    label: "Website Development",
+  },
+  {
+    value: "migration",
+    label: "Website Migration",
+  },
+  {
+    value: "consultation_frontend",
+    label: "Frontend Consultation",
+  },
+  {
+    value: "consultation_backend",
+    label: "Backend Consultation",
+  },
+  {
+    value: "ecommerce",
+    label: "E-commerce Site",
+  },
+  {
+    value: "performance",
+    label: "Performance Evaluation",
+  },
+];
+
+export const budgetData = [
+  "Less than $2,000",
+  "$2,000 - $10,000",
+  "More than $10,000",
+];
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -5,6 +5,14 @@ import styles from "../styles/pages/Contact.module.css";
 
 import PageTransition from "../components/PageTransition";
 // import Reveal from "../components/Reveal";
+import {
+  contactData,
+  infoData,
+  serviceData,
+  budgetData,
+} from "../data/contact";
+
+import { Link } from "react-router-dom";
 
 export default function ContactPage() {
   return (
@@ -15,7 +23,119 @@ export default function ContactPage() {
           <div className={styles.card}>
             <div className={styles.cardContent}>
               <div className={styles.details}>
-                hi
+                <div className={styles.contactSection}>
+                  <p className={styles.mainhead}>Get in touch</p>
+                  <p className={styles.subhead}>
+                    Let's talk about your project
+                  </p>
+                  <p className={styles.subtitle}>
+                    Thinking about a new project, a problem to solve, or just
+                    want to connect? Let's do it!
+                    <br />
+                    <br />
+                    <br />
+                    Use the form on this page or get in touch by other means.
+                  </p>
+                  <div className={styles.contactDetails}>
+                    <div className={styles.contactsContainer}>
+                      {contactData?.map((data) => {
+                        return (
+                          <div key={data.type} className={styles.contacts}>
+                            <div className={styles.logoContainer}>
+                              <img
+                                className={styles.logo}
+                                src={data.logo}
+                                alt="phone"
+                              />
+                            </div>
+                            {data.link ? (
+                              <Link
+                                to={data.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                <p
+                                  id={styles.clickable}
+                                  className={styles.detail}
+                                >
+                                  {data.type} : {data.detail}
+                                </p>
+                              </Link>
+                            ) : (
+                              <p className={styles.detail}>
+                                {data.type} : {data.detail}
+                              </p>
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.formSection}>
+                  <form>
+                    <p className={styles.heading}>Your contact information</p>
+                    <div className={styles.infoContainer}>
+                      {infoData?.map((data) => {
+                        return (
+                          <input
+                            key={data.name}
+                            type="text"
+                            className={styles.info}
+                            placeholder={data.placeholder}
+                            name={data.name}
+                            required
+                          />
+                        );
+                      })}
+                    </div>
+                    <p className={styles.heading}>
+                      What services do you need for your project?
+                    </p>
+                    <div className={styles.serviceContainer}>
+                      {serviceData?.map((data) => {
+                        return (
+                          <div key={data.label} className={styles.serviceGroup}>
+                            <input
+                              type="checkbox"
+                              name="services[]"
+                              value={data.value}
+                            />
+                            <label className={styles.label}>{data.label}</label>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <p className={styles.heading}>
+                      How much is the anticipated budget?
+                    </p>
+                    <div className={styles.budgetContainer}>
+                      {budgetData?.map((data) => {
+                        return (
+                          <div key={data} className={styles.radioGroup}>
+                            <input type="radio" name="budget" required />
+                            <label className={styles.label}>{data}</label>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <p className={styles.heading}>Tell me about your project</p>
+                    <div className={styles.descriptionContainer}>
+                      <textarea
+                        placeholder="Project description"
+                        name="description"
+                        rows="6"
+                        required
+                      />
+                    </div>
+
+                    <div className={styles.buttonContainer}>
+                      <button className={styles.button} type="submit">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
