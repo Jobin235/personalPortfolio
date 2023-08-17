f3d98bd835ebd46ed36736fce39c2ca551c92985
jobinjoy <jobinjoy235@gmail.com>
2023-08-17 11:06:00 +0530

    responsive client

--- src/pages/ClientsPage.js
+++ src/pages/ClientsPage.js
@@ -6,6 +6,8 @@ import styles from "../styles/pages/Client.module.css";
 import PageTransition from "../components/PageTransition";
 import { data } from "../data/clients";
 
+import Reveal from "../components/Reveal";
+
 export default function ClientsPage() {
   return (
     <div className={styles.container}>
@@ -15,17 +17,23 @@ export default function ClientsPage() {
           <div className={styles.card}>
             <div className={styles.cardContent}>
               <div className={styles.details}>
-                <div className={styles.subText}>Some of the brands I have worked with</div>
+                <div className={styles.subText}>
+                  Some of the brands I have worked with
+                </div>
                 <div className={styles.clients}>
                   {data.map((client) => {
                     return (
-                      <div className={styles.imageContainer}>
-                        <img
-                          className={styles.image}
-                          src={client.logo}
-                          alt={client.name}
-                        />
-                      </div>
+                      <React.Fragment key={client.name}>
+                        <Reveal>
+                          <div className={styles.imageContainer}>
+                            <img
+                              className={styles.image}
+                              src={client.logo}
+                              alt={client.name}
+                            />
+                          </div>
+                        </Reveal>
+                      </React.Fragment>
                     );
                   })}
                 </div>
